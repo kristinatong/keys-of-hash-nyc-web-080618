de6179a2e30852580861344f2e8ddf3614dd9fa2
Kristina Tong <kristina.tong@gmail.com>
2018-08-07 14:23:36 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,17 @@
 class Hash
   def keys_of(arguments)
-    # code goes here
+    data = {
+  "Panama" => ["red-footed tortoise"], 
+  "Madagascar" => ["aye-aye", "tomato frog"],
+  "Australia" => ["sugar glider", "kangaroo", "koala"]
+  }
+    array = []
+  
+    data.each do |x,y|
+      if x == arguments
+        array << y
+      end
+    end
+    array.flatten
   end
 end
